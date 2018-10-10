5462a6ae6969cc0a0bd0e2c8b922015c5f8d145b
Deonte Williams <icode4lyfe@gmail.com>
2018-10-09 22:14:01 -0400

    extracts callback for error messages and result

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,18 +1,20 @@
 const request = require("request");
 
-let geocodeAddress = (address) => {
+let geocodeAddress = (address, callback) => {
    request({
       url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=AIzaSyC3xdoA_ABtNNTLFrbuZ2sdfnzhpdxgTdk`,
       json: true
    }, (error, response, body) => {
       if (error) {
-         console.log("Unable to connect to Google servers.");
+         callback("Unable to connect to Google servers.");
       } else if (body.status === "ZERO_RESULTS") {
-         console.log("Unable to find that address.");
+         callback("Unable to find that address.");
       } else if (body.status === "OK") {
-         console.log(`Address: ${body.results[0].formatted_address}`);
-         console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-         console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+         callback(undefined, {
+            address: body.results[0].formatted_address,
+            latitude: body.results[0].geometry.location.lat,
+            longitude: body.results[0].geometry.location.lng
+         });
       }
    });
 };
